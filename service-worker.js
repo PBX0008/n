5d4da6e4e1e6d73f165bfb69f1d27919a74baadb
PBX0008 <pattimultani@gmail.com>
2026-08-09 17:43:39 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_VERSION = 'pbx-nclex-pwa-v10';
+const CACHE_VERSION = 'pbx-nclex-pwa-v11';
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const APP_SHELL = [
